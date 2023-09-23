bbd632168abb8bab33f59140b51cc1a2f3fe0b89
Giacomo Schott <giacomoschottaaq@gmail.com>
2023-09-23 20:13:17 +0200

    initial commit

---
+++ csvdjango/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import HttpResponse
+import pandas as pd
+  
+def Table(request):
+    df = pd.read_csv("foo.csv")
+    #'tableview/static/csv/20_Startups.csv' is the django 
+    # directory where csv file exist.
+    # Manipulate DataFrame using to_html() function
+    geeks_object = df.to_html()
+  
+    return HttpResponse(geeks_object)
---
+++ readme.md
@@ -0,0 +1,3 @@
+<h1>Dependencies</h1>
+Runs on django
+Python dependencies: pandas
